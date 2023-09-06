2c548d483044a0e0d3f837393c177b46d75e89de
Dan7h3x <m.jalili.barbin@gmail.com>
2023-09-06 15:16:09 +0330

    --wip-- [skip ci]

--- lua/NvimPy/Lazy.lua
+++ lua/NvimPy/Lazy.lua
@@ -460,6 +460,33 @@ require("lazy").setup({
 		end,
 	},
 	{ "kosayoda/nvim-lightbulb" },
-
+	{
+		"GCBallesteros/NotebookNavigator.nvim",
+		keys = {
+			{
+				"]h",
+				function()
+					require("notebook-navigator").move_cell("d")
+				end,
+			},
+			{
+				"[h",
+				function()
+					require("notebook-navigator").move_cell("u")
+				end,
+			},
+			{ "<leader>X", "<cmd>lua require('notebook-navigator').run_cell()<cr>" },
+			{ "<leader>x", "<cmd>lua require('notebook-navigator').run_and_move()<cr>" },
+		},
+		dependencies = {
+			"echasnovski/mini.comment",
+			"anuvyklack/hydra.nvim",
+		},
+		event = "VeryLazy",
+		config = function()
+			local nn = require("notebook-navigator")
+			nn.setup({ activate_hydra_keys = "<leader>h" })
+		end,
+	},
 	{ import = "NvimPy.Extra.debug" },
 })
