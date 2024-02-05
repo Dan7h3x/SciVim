9744cb824b7dd5529269673213ce54a466785b84
Dan7h3x <m.jalili.barbin@gmail.com>
2024-02-05 19:50:52 +0330

    Updated Lualine

--- lua/NvimPy/FormatLazy.lua
+++ lua/NvimPy/FormatLazy.lua
@@ -1,28 +1,42 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		config = function()
-			require("conform").setup({
-				formatters_by_ft = {
-					lua = { "stylua" },
-					-- Conform will run multiple formatters sequentially
-					python = { "isort", "ruff_format" },
-					-- Use a sub-list to run only the first available formatter
-					javascript = { { "prettierd", "prettier" } },
-					typst = { "typstfmt" },
-					latex = { "latexindent" },
-				},
-				format_on_save = {
-					-- These options will be passed to conform.format()
-					timeout_ms = 500,
-					lsp_fallback = true,
-				},
-			})
-		end,
-	},
-	{
-		"danymat/neogen",
-		dependencies = "nvim-treesitter/nvim-treesitter",
-		config = true,
-	},
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          ["javascript"] = { "prettier" },
+          ["javascriptreact"] = { "prettier" },
+          ["typescript"] = { "prettier" },
+          ["typescriptreact"] = { "prettier" },
+          ["vue"] = { "prettier" },
+          ["css"] = { "prettier" },
+          ["scss"] = { "prettier" },
+          ["less"] = { "prettier" },
+          ["html"] = { "prettier" },
+          ["json"] = { "prettier" },
+          ["jsonc"] = { "prettier" },
+          ["yaml"] = { "prettier" },
+          ["markdown"] = { "prettier" },
+          ["markdown.mdx"] = { "prettier" },
+          ["graphql"] = { "prettier" },
+          ["handlebars"] = { "prettier" },
+          ["lua"] = { "stylua" },
+          ["python"] = { "isort", "ruff_format" },
+          ["typst"] = { "typstfmt" },
+          ["latex"] = { "latexindent" },
+        },
+
+        format_on_save = {
+          -- These options will be passed to conform.format()
+          timeout_ms = 500,
+          lsp_fallback = true,
+        },
+      })
+    end,
+  },
+  {
+    "danymat/neogen",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = true,
+  },
 }
