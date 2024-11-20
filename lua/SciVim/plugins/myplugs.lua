18f39cfaada2227512e471c19441905432bd92ef
Dan7h3x <m.jalili.barbin@gmail.com>
2024-11-20 18:52:38 +0330

    Lsp_signature --> signup.nvim

--- lua/SciVim/plugins/myplugs.lua
+++ lua/SciVim/plugins/myplugs.lua
@@ -1,30 +1,31 @@
 return {
-  -- {
-  --   "Dan7h3x/signup.nvim",
-  --   event = "LspAttach",
-  --   config = function()
-  --     require("signup").setup(
-  --     -- Your configuration options here
-  --       {
-  --         silent = true,
-  --         number = true,
-  --         icons = {
-  --           parameter = " ",
-  --           method = " ",
-  --           documentation = " ",
-  --         },
-  --         colors = {
-  --           parameter = "#86e1fc",
-  --           method = "#c099ff",
-  --           documentation = "#4fd6be",
-  --         },
-  --         border = "rounded",
-  --         winblend = 5,
-  --         override = true, -- Override default LSP handler for signatureHelp
-  --       }
-  --     )
-  --   end
-  -- }
+  {
+    "Dan7h3x/signup.nvim",
+    branch = "devel",
+    event = "LspAttach",
+    config = function()
+      require("signup").setup(
+      -- Your configuration options here
+        {
+          silent = false,
+          number = true,
+          icons = {
+            parameter = " ",
+            method = " ",
+            documentation = " ",
+          },
+          colors = {
+            parameter = "#86e1fc",
+            method = "#c099ff",
+            documentation = "#4fd6be",
+          },
+          border = "shadow",
+          winblend = 10,
+          override = false, -- Override default LSP handler for signatureHelp
+        }
+      )
+    end
+  },
   {
     "Dan7h3x/neaterm.nvim",
     branch = "stable",
