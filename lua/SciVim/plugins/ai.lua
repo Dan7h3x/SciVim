90f166e093d43de8cbcbb7cdb824398c50b87508
Dan7h3x <m.jalili.barbin@gmail.com>
2024-12-07 19:38:18 +0330

    Updated Configuration

--- lua/SciVim/plugins/ai.lua
+++ lua/SciVim/plugins/ai.lua
@@ -1,6 +1,7 @@
 return {
   {
-    dir = "~/.config/nvim/SciVim/chatter", -- Your chat plugin
+    "Dan7h3x/chatter.nvim",
+    branch = "devel",
     lazy = true,
     dependencies = {
       "ibhagwan/fzf-lua", lazy = true,
@@ -9,61 +10,34 @@ return {
       "<leader>cc", "<Cmd>ChatterStart<CR>", desc = "Chatter Start"
     }, },
     config = function()
-      require('SciVim.chatter').setup({
-        offline_api_url = os.getenv("OLLAMA_HOST") or "http://localhost:11434",
-        sidebar_width = 60,
-        sidebar_height = vim.o.lines - 12,
-        models = {},
-        highlight = {
-          title = "Title",
-          user = "Comment",
-          assistant = "String",
-          system = "Type",
-          error = "ErrorMsg",
-          loading = "WarningMsg",
+      require('SciVim.chatter').setup(
+        {
+          offline_api_url = os.getenv("OLLAMA_HOST") or "http://localhost:8888",
+          sidebar_width = 60,
+          sidebar_height = vim.o.lines - 12,
+          models = {},
+          highlight = {
+            title = "Title",
+            user = "Comment",
+            assistant = "String",
+            system = "Type",
+            error = "ErrorMsg",
+            loading = "WarningMsg", -- New highlight group for loading animation
 
-        }
-      })
+          },
+          features = {
+            inline_completion = true,
+            code_actions = true,
+            context_awareness = true,
+            auto_import_suggestions = true,
+          },
+          keymaps = {
+            inline_completion = '<C-space>',
+            code_actions = '<leader>ca',
+            explain_code = '<leader>ce',
+            generate_docs = '<leader>cd',
+          }
+        })
     end,
   },
-
-  -- {
-  --   dir = "~/.config/nvim/lua/SciVim/gptvim/init.lua",
-  --   config = function()
-  --     require("SciVim.gptvim").setup({
-  --       api_key = "",
-  --       model = "gpt-4",
-  --     })
-  --   end
-  -- },
-
-  -- {
-  --   "Dan7h3x/chatter.nvim",
-  --   event = "VeryLazy",
-  --   dependencies = {
-  --     'nvim-lua/plenary.nvim',
-  --     "ibhagwan/fzf-lua",
-  --   },
-  --   keys = { {
-  --     "<leader>cc", "<Cmd>ChatterStart<CR>", desc = "Chatter Start"
-  --   }, },
-  --   config = function()
-  --     require('chatter').setup({
-  --       offline_api_url = os.getenv("OLLAMA_HOST") or "http://localhost:8888",
-  --       sidebar_width = 60,
-  --       sidebar_height = vim.o.lines - 12,
-  --       models = {},
-  --       highlight = {
-  --         title = "Title",
-  --         user = "Comment",
-  --         assistant = "String",
-  --         system = "Type",
-  --         error = "ErrorMsg",
-  --         loading = "WarningMsg",
-  --
-  --       }
-  --     })
-  --   end,
-  -- }
-
 }
